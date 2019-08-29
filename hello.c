801f21e5589224b22bfae05ebc3747583459dfa6
aalfazari2 <ahmed1998.ahmed1998.aa@gmail.com>
2019-08-29 13:12:19 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ahmed Alfazari, Griffin Brow
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ahmed Alfazari, Griffin Brow\n");
+
+  return 0;
+}
